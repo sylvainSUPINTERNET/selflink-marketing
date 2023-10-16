6670374806a0a6d6c691692536e272705a3351c3
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-10-17 00:02:03 +0200

    [WIP]

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 from typing import List
-from services.youtube.youtube import build_tree_microinfluenceur
+from repositories.YoutubeInfluenceur import YoutubeInfluenceur
 from dotenv import load_dotenv
+
+from services.youtube.youtube import get_categories, get_influenceur, get_subcategory
 load_dotenv()
 
-THEMES:List[str] = ["lifestyle", "formation", "spiritual"]
 
 def main():
-    build_tree_microinfluenceur(sheets_name_list=THEMES)
+    cat_list = get_categories()
+    subcat_list = get_subcategory()
+    influenceur_list = get_influenceur()
+    
+    youtube_influenceur = YoutubeInfluenceur()
+    
+    youtube_influenceur.build_tree_add_node_category()
+    youtube_influenceur.build_tree_add_node_subcategory()
+    
+    youtube_influenceur.insert_category(cat_list)
+    youtube_influenceur.insert_subcategory(subcat_list)
+    
+    for influenceur in influenceur_list:
+        youtube_influenceur.insert_influenceur(influenceur)
+    
+    youtube_influenceur.close()
 
 if __name__ == "__main__":
     main()
--- src/services/youtube/youtube.py
+++ src/services/youtube/youtube.py
@@ -3,13 +3,39 @@ import pandas as pd
 import os
 from logger.logger import logging
 
-def build_tree_microinfluenceur(sheets_name_list:List[str]):
+
+
+
+def get_categories() -> List[str]:
+    current_path = os.path.dirname(f"{os.path.abspath(__file__)}")
+    all_sheets_df = pd.read_excel(current_path+"/micro_influenceur.xlsx", sheet_name=None)
+    return list(all_sheets_df.keys())
+
+def get_subcategory() -> List[str]:
     current_path = os.path.dirname(f"{os.path.abspath(__file__)}")
     all_sheets_df = pd.read_excel(current_path+"/micro_influenceur.xlsx", sheet_name=None)
     
+    m = set()
+    for (_, df) in all_sheets_df.items():
+        m.update(df["sub_category"].unique())
+    return list(m)
+
+def get_influenceur() -> List[str]:
+    current_path = os.path.dirname(f"{os.path.abspath(__file__)}")
+    all_sheets_df = pd.read_excel(current_path+"/micro_influenceur.xlsx", sheet_name=None)
     
-    for (sheet_name, df) in all_sheets_df.items():
+    m = set()
+    for (_, df) in all_sheets_df.items():
+        m.update(df["channel_name"].unique())
+    return list(m)
+
+# def build_tree_microinfluenceur(sheets_name_list:List[str]):
+#     current_path = os.path.dirname(f"{os.path.abspath(__file__)}")
+#     all_sheets_df = pd.read_excel(current_path+"/micro_influenceur.xlsx", sheet_name=None)
+    
+    
+#     for (sheet_name, df) in all_sheets_df.items():
         
-        if sheet_name in sheets_name_list:
-            logging.info("sheet_name: %s", sheet_name)
+#         if sheet_name in sheets_name_list:
+#             logging.info("sheet_name: %s", sheet_name)
                 
